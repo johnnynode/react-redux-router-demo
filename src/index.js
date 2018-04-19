28b066165af95cd54d69386db4425a3736361c3c
johnnynode <johnnynode@gmail.com>
2018-04-19 12:50:08 +0800

    add Services

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './Services/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
