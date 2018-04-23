f5aca4467cf02456799e37d036021ac8c505dbeb
johnnynode <johnnynode@gmail.com>
2018-04-23 13:36:10 +0800

    add fast click dependency

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'; // 通过 react-redux 来实现store和ui组件的连接
+import FastClick from 'fastclick';
 import registerServiceWorker from './services/registerServiceWorker';
 import AppRouter from '@/router';
 import Store from '@/store';
@@ -13,3 +14,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 registerServiceWorker();
+FastClick.attach(document.body); // 使用FastClick工具解决click事件300ms延时的问题
