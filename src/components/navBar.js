c4271a874b12ea6ce7519a5f880a3ef74fb62736
johnnynode <johnnynode@gmail.com>
2018-05-02 08:41:50 +0800

    add navBar style and css style

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import './navBar.css';
 
 export default class NavBar extends Component {
 	render() {
 		return (
-			<ul>
-				<li><NavLink to="/">首页</NavLink></li>
-				<li><NavLink to={{pathname:"/list", search:"?category=0101"}}>列表页</NavLink></li>
-				<li><NavLink to="/about">关于页</NavLink></li>
+			<ul className="clearfix nav-bar">
+				<li className="left"><NavLink to="/">首页</NavLink></li>
+				<li className="left"><NavLink to={{pathname:"/list", search:"?category=0101"}}>列表页</NavLink></li>
+				<li className="left"><NavLink to="/about">关于页</NavLink></li>
 			</ul>
 		);
 	}
