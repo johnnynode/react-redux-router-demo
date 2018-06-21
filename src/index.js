28b2301e7689529b22f3a341f886fd2ee6be5d13
johnnynode <johnnynode@gmail.com>
2018-06-21 23:15:40 +0800

    test github cheat commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'; // 通过 react-redux 来实现store和ui组件的连接
-// import FastClick from 'fastclick';
+import FastClick from 'fastclick';
 import registerServiceWorker from './services/registerServiceWorker';
 import AppRouter from '@/router';
 import Store from '@/store';
@@ -14,4 +14,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 registerServiceWorker();
-// FastClick.attach(document.body); // 使用FastClick工具解决click事件300ms延时的问题
+FastClick.attach(document.body); // 使用FastClick工具解决click事件300ms延时的问题
